37e0dadf29bec17d6922d2919385a828d3eb3612
jarb29 <jarb29@gmail.com>
2020-05-03 16:33:09 -0400

    flux

---
+++ src/flux.js
@@ -0,0 +1,20 @@
+const getState = ({ getStore, getActions, setStore }) => {
+  return {
+    // base datos Angel
+    store: {
+      ///// Alex URL
+      baseURL: ""
+
+     
+	},
+	
+
+    actions: {
+      /////// Funcion de autenticacion para el login
+
+      
+    }
+  };
+};
+
+export default getState;
