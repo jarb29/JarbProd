41bbda6b975cf825088825edd6592c118750a452
jarb29 <jarb29@gmail.com>
2020-05-23 14:40:27 -0400

    grafica

--- src/components/Corte/VistaGeneral/CorteGrafica.js
+++ src/components/Corte/VistaGeneral/CorteGrafica.js
@@ -4,6 +4,13 @@ import { Context } from "../../../AppContext";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import CardBody from "components/Card/CardBody.js";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 export default function CorteGrafica() {
   const { store } = useContext(Context);
@@ -15,6 +22,7 @@ export default function CorteGrafica() {
   const valores = Object.values(store.tiempoEstufa);
   const valores_diarios = Object.values(store.tiempo_diario_estufa);
   const valores_tiempo = [];
+
   const valores_diarios_estufa = [];
 
   const valoresPorPieza = valores.map(valor => {
@@ -28,6 +36,23 @@ export default function CorteGrafica() {
       valores_diarios_estufa.push(val.total_suma);
     });
   });
+
+  console.log(valores_diarios_estufa, "valores diario");
+  console.log(valores_tiempo, "valores tiempo");
+
+  const nuevo_modelos_usados = [];
+
+  const a = modelos.map((modelo, index) => {
+    const data = {
+      modelo: modelo,
+      tiempo_faltate: valores_tiempo[index] - valores_diarios_estufa[index],
+      tiempo_total: valores_tiempo[index],
+      tiempo_cortado: valores_diarios_estufa[index]
+    };
+    return nuevo_modelos_usados.push(data);
+  });
+
+  console.log(nuevo_modelos_usados, "veamos que sale");
 
   const multipleBarsChart = {
     data: {
@@ -82,6 +107,30 @@ export default function CorteGrafica() {
             listener={multipleBarsChart.animation}
           />
         </CardBody>
+        <TableContainer component={Paper}>
+          <Table size="small" aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="left">Modelo</TableCell>
+                <TableCell align="left">Tiempo Faltante (minutos)</TableCell>
+                <TableCell align="left">Tiempo total (minutos)</TableCell>
+                <TableCell align="left">Tiempo Consumido (minutos)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {nuevo_modelos_usados.map((model, index) => {
+                return (
+                  <TableRow key={index}>
+                    <TableCell component="th">{model.modelo}</TableCell>
+                    <TableCell align="left"> {model.tiempo_faltate}</TableCell>
+                    <TableCell align="left"> {model.tiempo_total}</TableCell>
+                    <TableCell align="left"> {model.tiempo_cortado}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </GridItem>
     </GridContainer>
   );
